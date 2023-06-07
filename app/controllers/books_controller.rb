463f62e50ab2a56196e143185612d96bd2e2b395
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-06-07 22:56:01 +0000

    [finish] 必要なモデルの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,23 @@ class BooksController < ApplicationController
   def new
     @books = Books.new
   end
+  
+  def create
+    @books = Book.new(books_params)
+    @books.user_id = current_user.id
+    @books.save
+    redirect_to books_path
+  end
 
   def index
   end
 
   def show
+  end
+  
+  private
+  
+  def books_params
+    params.require(:books).permit(:title, :body)
   end
 end
