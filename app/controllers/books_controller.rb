cd8a3f2ec4937c16b88f06bc6774d4e3f1af7d47
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-06-10 08:00:56 +0000

    投稿、編集機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,27 +2,28 @@ class BooksController < ApplicationController
   def new
     @books = Books.new
   end
-  
+
   def create
     @books = Book.new(books_params)
     @books.user_id = current_user.id
     @books.save
     redirect_to books_path
   end
-  
+
   def edit
-    
+
   end
 
   def index
+    @user = current_user
   end
 
   def show
   end
-  
+
   private
-  
+
   def books_params
-    params.require(:books).permit(:title, :body)
+    params.require(:books).permit(:title, :body, :image)
   end
 end
